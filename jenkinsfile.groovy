ef7a73a4f3046df081f9a47d990120b29dffdcee
Dastan Kadyrkulov <dastankadyrkulov@Dastans-MacBook-Pro.local>
2019-05-04 16:42:11 -0500

    test

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -2,12 +2,13 @@ node{
     properties([parameters([string(defaultValue: 'IP', description: 'Where to build e.g IP', name: 'ENV', trim: true)])])
     stage("Clone repo"){
         git 'git@github.com:Nurjan87/Flaskex.git'
-    }
+     }
     stage("Remove /tmp content"){
         sh "ssh ec2-user@${ENV} sudo rm -rf /tmp/*"
     }
     stage("Copy filese over"){
         sh "scp -r * ec2-user@${ENV}:/tmp"
+        sh "ssh ec2-user@${ENV}  sudo pip install -r /tmp/requirements.txt"
     }
     stage("Create Folder"){        
         sh "ssh ec2-user@${ENV} sudo mkdir -p /flaskex"
